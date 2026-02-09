a1c993846de7420518072e1b322d3a350a34e49b
Nino <ninonenni@macbookpro.home>
2026-02-09 23:16:09 +0100

    fixed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maganino.github.io',
+  base: '/maganino/',
+
   vite: {
     plugins: [tailwindcss()]
   }
